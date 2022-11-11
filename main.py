aa848f5182d39f386c6b04c67c8c6132f8635848
Christopher Cialone <ccialone0929@gmail.com>
2022-11-11 11:52:08 -0500

    pivoted directions, pending unit tests

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
-# print "welcome to the tip-calculator" message
-print("Welcome to christopher_cialone's tip-calculator")
-
-
-
-
-# variable meal equals an input as a float with text "enter the total bill"
-meal = float(input('Enter the cost of meal $: '))
-# variable tip equals an input the user chooses: 10, 15, 12....
-tip = int(input('Enter the tip %: '))
-# Constant variable added to every transaction - sales tax doesn't change
-tax = int(input('Enter the state sales tax %: '))
-ppl_splitting = int(input('Enter the number of people paying : '))
+# tip_calculator
+def cialone_python_calculator():
+  
+  # print message
+  print("Welcome to christopher_cialone's tip-calculator")
+  # variable meal set to what the user inputs
+  meal = float(input('Enter the cost of meal $: ')) # must be a number or code breaks. if code breaks prompt user to enter an integer
+  # variable tip equals set to an input of the user choice
+  tip = float(input('Enter the tip %: '))
+  # Could have hard-coded tip to equal 10%
+  tax = float(input('Enter the state sales tax %: '))
+  # variable ppl_splitting set to the number of people paying 
+  ppl_splitting = int(input('Enter the number of people paying : '))
 
 # calculations
-# tip_amt is equal what the meal cost is multiplied by the number represented tip divided by 100
-tip_amt = meal * tip/100
-tax_amount = meal * tax/100
-total = meal + tax_amount + tip_amt
-total_per_person = total/ppl_splitting
+ 
+  tip_amt = meal * tip/100 
+# from % to $ to leave as tip
+  tax_amount = meal * tax/100 
+#the amount the tax came to
+  total = meal + tax_amount + tip_amt 
+# adding your cost of meal, and the taxes, and the amount of money left for tip (everything) together, is the total
+  total_per_person = total/ppl_splitting # the total bill(including taxes and tip) divided by the number of people. Even-Stevens
+  
+  print(f"\nYour meal was ${meal:.2f} and your tip was ${tip_amt:.2f}.")
+  print(f"Your total with tax was ${total:.2f}.\n")
+  print(f"\nEach person paying will pay ${total_per_person:.2f}.")
 
-print(f"\nYour meal was ${meal:.2f} and your tip was ${tip_amt:.2f}.")
-print(f"Your total with tax was ${total:.2f}.\n")
-print(f"\nEach person paying will pay ${total_per_person:.2f}.")
-
-# create a function
-# if else loop
+# 
+# this statement is giving the option to the user, whether or not they would like to continue on and calculate more
+  the_next_tip = input("Press n to exit or continue on by entering any number ): ')
+  print(the_next_tip)
+  for answer in the_next_tip:
+      if answer == 'n': # 'n' is the exit from our program
+         print('Thanks for stopping by, may the force be with you')
+         return  
+      else:
+          the_next_tip == 'y'
+          cialone_python_calculator()
+        # or else the function calculates more meals and tips! 
+cialone_python_calculator()
